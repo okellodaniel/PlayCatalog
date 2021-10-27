83e8c9865a71f5c332768b7bff128bc8f196b6c9
Daniel Okello <handsdani@gmail.com>
2021-10-27 08:09:20 +0300

    Dto changes

--- PLAY.CATALOG/src/Play.Catalog.Service/Dtos.cs
+++ PLAY.CATALOG/src/Play.Catalog.Service/Dtos.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations;
 namespace Place.Catalog.Service.Dtos
 {
     public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
-
     public record CreatedItemDto([Required] string Name, string Description, [Range(0, 5000)] decimal Price);
     public record UpdateItemDto(string Name, string Description, decimal Price);
 }
